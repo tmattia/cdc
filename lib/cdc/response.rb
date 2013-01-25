4dd2c918c39f70a30892e96b5ea93f963c91acaa
Tomas Mattia <tmattia@thoughtworks.com>
2013-01-25 19:41:08 -0200

    Updated response matching to use Hash#subset_of?

--- lib/cdc/response.rb
+++ lib/cdc/response.rb
@@ -8,7 +8,7 @@ module CDC
 
     def matches?(response)
       valid_status = @status == response.status
-      valid_headers = @headers == response.headers.select { |key,_| @headers.include?(key) }
+      valid_headers = @headers.subset_of?(response.headers)
       valid_body = @schema.validate(response.body)
       valid_status && valid_headers && valid_body
     end
